fda28ec2587e87d7999ab7fa9bf7cb6e184a67d7
Luka Stojadinovic <80914501+Lukiano99@users.noreply.github.com>
2024-10-01 06:44:52 +0200

    Update post.ts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,5 +3,6 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 export const postRouter = createTRPCRouter({
   helloPublic: publicProcedure.query(async ({ ctx }) => {
     return await ctx.db.users.findMany({ where: {} });
+console.log({ctx});
   }),
 });
